b3e56aa85b80f9e7e0f755688c5de873b84ac68d
kanape11 <88274731+kanape11@users.noreply.github.com>
2021-12-04 08:32:56 +0700

    Add files via upload

---
+++ In Air Fake Amount.lua
@@ -0,0 +1,25 @@
+local config = fatality.config
+local menu = fatality.menu
+local input = fatality.input
+local key = 0x20 --space [https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes]
+local amountf = menu:get_reference("Rage", "Anti-aim", "General", "Fake Amount")
+
+local fake_amount_item = config:add_item( "Fake Amount (In Air)", 0.0 )
+local fake_amount_slider = menu:add_slider( "Fake Amount (In Air)", "Rage", "Anti-aim", "fakelag", fake_amount_item, -100, 100, 1 )
+
+local function SetFakeAmount(fakeAmount) 
+
+amountf:set_int(input:is_key_down(key) and fakeAmount or '20')
+
+end
+
+function on_paint( )
+
+    local Famount = fake_amount_item:get_int()
+
+    SetFakeAmount(Famount)
+
+end
+
+local callbacks = fatality.callbacks
+callbacks:add( "paint", on_paint )
